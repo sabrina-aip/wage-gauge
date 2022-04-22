9482dff4264ae224430e9f29dbb99d1a4f7e4393
sabrina-aip <93457861+sabrina-aip@users.noreply.github.com>
2022-04-22 10:14:14 -0400

    i grow tired
    it's my birthday bro why am i trying to figure out how to make a custom legend

--- canada-chart.js
+++ canada-chart.js
@@ -14,24 +14,23 @@ Object.keys(CAN_DATA).forEach((occupation)=>{
     })
 })
 
-
 var datasets = []
 
 Object.keys(data).forEach((occupation)=>{
     dataPoint = {};
-    dataPoint['hidden'] = true;
+    //dataPoint['hidden'] = true;
     dataPoint['label'] = occupation;
-    dataPoint['lineTension'] = false;
+    dataPoint['lineTension'] = .1;
     dataPoint['fill'] = true;
     dataPoint['backgroundColor'] = 'rgba(146,220,88,.01)';
     dataPoint['borderColor'] = 'rgba(146,220,88,1)';
-    dataPoint['pointRadius'] = 10;
+    dataPoint['pointRadius'] = 3;
     dataPoint['pointBackgroundColor'] = 'rgba(146,220,88,1)';
     dataPoint['pointBorderColor'] = 'rgba(146,220,88,1)';
     dataPoint['pointHoverRadius'] = 10;
     dataPoint['pointHoverBackgroundColor'] = 'rgba(146,220,88,1)';
     dataPoint['pointHitRadius'] = 50;
-    dataPoint['pointBorderWidth'] = 2;
+    dataPoint['pointBorderWidth'] = 1;
     dataPoint['data'] = data[occupation];
     datasets.push(dataPoint)
 })
@@ -68,8 +67,34 @@ var line = new Chart(ctx, {
         display: true,
         labels: {
             fontColor: 'white'
+        },
+        onClick: function(e, legendItem) {
+          var index = legendItem.datasetIndex;
+          var ci = this.chart;
+          var alreadyHidden = (ci.getDatasetMeta(index).hidden === null) ? false : ci.getDatasetMeta(index).hidden;
+  
+          ci.data.datasets.forEach(function(e, i) {
+            var meta = ci.getDatasetMeta(i);
+  
+            if (i !== index) {
+              if (!alreadyHidden) {
+                meta.hidden = meta.hidden === null ? !meta.hidden : null;
+              } else if (meta.hidden === null) {
+                meta.hidden = true;
+              }
+            } else if (i === index) {
+              meta.hidden = null;
+            }
+          });
         }
-        
       }
     }
   });
+
+const legendBox = document.querySelector('.legend-container')
+Object.keys(data).forEach((occupation)=>{
+  btn = document.createElement('button')
+  btn.innerHTML = occupation
+  console.log(btn)
+  legendBox.appendChild(btn)
+})

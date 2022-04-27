71866597172ca16ab1f21fd4d60390372d30f9f6
Chris Johnston <chris@thejohnstons.net>
2022-04-26 21:54:31 -0700

    fix: pad zeroes for minute and second in results page

--- result.js
+++ result.js
@@ -1,6 +1,6 @@
 const hour = sessionStorage.getItem('hour')
-const minute = sessionStorage.getItem('minute')
-const second = sessionStorage.getItem('second')
+const minute = sessionStorage.getItem('minute').padStart(2, '0')
+const second = sessionStorage.getItem('second').padStart(2, '0')
 const score = sessionStorage.getItem('score')
 
 const resultStatement = document.querySelector('.result-statement')
